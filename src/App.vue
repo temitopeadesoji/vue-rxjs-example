93d216ea919faee2354e0037c54168472bf09aeb
Temitope <temitopeadesoji23@gmail.com>
2021-06-17 00:55:11 +0100

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <RxJs />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RxJs from "./components/RxJs.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    RxJs,
+  },
+};
 </script>
 
 <style>
